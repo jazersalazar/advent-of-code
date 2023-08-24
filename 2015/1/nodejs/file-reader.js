6dd8e70067f8edcecb5b188ec1cba2320db51552
Jazer Salazar <48846249+jazersalazar@users.noreply.github.com>
2023-08-24 10:35:19 +0800

    refactor(core): add nodejs dir

--- 2015/1/nodejs/file-reader.js
+++ 2015/1/nodejs/file-reader.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 const input = fs
-	.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
+	.readFileSync(path.join(__dirname, '../input.txt'), 'utf-8')
 	.toString()
 	.trim()
 	.split('')
